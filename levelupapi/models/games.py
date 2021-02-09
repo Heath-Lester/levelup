94ce94578094304b6f10f26b2fd00ff25fce035c
Heath Lester <h.allen.lester@gmail.com>
2021-02-09 17:04:59 -0600

    added game views and url routing

--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -5,6 +5,7 @@ class Game(models.Model):
 
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
+    maker = models.CharField(max_length=50)
     game_type = models.ForeignKey("Game_Type", on_delete=models.CASCADE)
     number_of_players = models.IntegerField()
-    description = models.CharField(max_length=50)
+    skill_level = models.IntegerField()
